e8126dfcdf62c8a4e714859266fb5ebb4120cd07
Pian Nottingham <124229236+Snooppian@users.noreply.github.com>
2023-02-15 08:16:04 +0700

    2

--- t1.py
+++ t1.py
@@ -3,5 +3,5 @@ from tkinter import *
 root = Tk()
 photo = PhotoImage(file="coffee.png")
 Button(root,image=photo,borderwidth=0).pack(side=LEFT,padx =20,pady=15)
-
+Label(root,text='dlldslskl').pack()
 root.mainloop()
